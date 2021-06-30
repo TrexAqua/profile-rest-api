6677902ecf537d1a48e857dc44ab8c615060c976
TrexAqua <mayankg92156@gmail.com>
2021-06-30 23:43:24 +0530

    Started Working with views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView Features"""
+        an_apiview = [
+            'Uses HTTP methods as functions (get,put,post,delete)',
+            'Is similar to a traditional django view',
+        ]
+        return Response({
+            "message": "Working For Now",
+            "Features": an_apiview
+        })
